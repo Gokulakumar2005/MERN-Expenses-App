511f2d9a1be8362c9b09735ee138a1a2e9a1a214
Gokulakumar <gokulakumar2005@gmail.com>
2026-06-10 00:01:56 +0530

    live error fixing

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080"||import.meta.env.VITE_API_URL 
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080"
 });
 
 axiosInstance.interceptors.request.use((config) => {
