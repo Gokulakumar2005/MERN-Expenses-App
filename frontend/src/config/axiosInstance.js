9f13bc1208534bffc713dbb33063ebf561cf4e7e
Gokulakumar <gokulakumar2005@gmail.com>
2026-06-09 22:56:21 +0530

    Deployment Changes

--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080"||"https://mern-expenses-app-backend.onrender.com"
+    baseURL: "http://localhost:8080"||import.meta.env.VITE_API_URL 
 });
 
 axiosInstance.interceptors.request.use((config) => {
