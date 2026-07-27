67704d57bc1b7898852f260b6d82465830e531e4
Gokulakumar <gokulakumar2005@gmail.com>
2026-07-27 21:52:12 +0530

    fixing the db url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const configureDB = async () => {
-    const dbUrl = process.env.DB_URL || "mongodb+srv://expensetracker_user:Gokulakumar2005@expensetracker.bibr3mx.mongodb.net/?appName=ExpenseTracker";
+    let dbUrl = process.env.DB_URL;
+    console.log("Connecting to MongoDB Atlas...");
     try {
         await mongoose.connect(dbUrl);
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
     }
